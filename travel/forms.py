b675dea562d6171dacf542194f995bdb336b3bc2
Prashantpandey2398 <pprashant2398@gmail.com>
2018-04-06 12:26:37 +0530

    second upda

---
+++ travel/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.contrib.auth.models import User
+from travel.models import UserProfile
+
+class UserForm(forms.ModelForm):
+    username = forms.CharField(max_length=100, required=True, help_text="Username",
+                               widget=forms.TextInput(attrs={'class': 'validate', 'input': 'text'}))
+    password = forms.CharField(widget=forms.PasswordInput(), required=True, help_text="Password")
+    email = forms.EmailField(max_length=100, required=True, help_text="Email",
+                             widget=forms.EmailInput(attrs={'class': 'validate', 'input': 'email'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+class UserProfileForm(forms.ModelForm):
+    name = forms.CharField(max_length=30, required=True, help_text="Name",
+                           widget=forms.TextInput(attrs={'class': 'validate', 'input': 'text'}))
+    contact = forms.CharField(required=True, help_text="Phone Number",
+                              widget=forms.TextInput(attrs={'class': 'validate', 'input': 'text', 'maxlength': '10'}))
+    college = forms.CharField(max_length=100, required=True, help_text="College Name",
+                              widget=forms.TextInput(attrs={'class': 'validate', 'input': 'text'}))
+    gender = forms.ChoiceField(choices=[('0', 'Female'), ('1', 'Male'), ('2', 'Other')])
+    picture = forms.ImageField(help_text='Profile Picture', required=False,
+                               widget=forms.FileInput(attrs={'type': 'file'}))
+    location = forms.CharField(choices=[('Gurgaon','Gurgaon'),('Kapriwas','Kapriwas'),('Manesar','Manesar')])
+
+    class Meta:
+        model = UserProfile
+        fields = ('name', 'contact', 'college', 'gender', 'picture', 'Terms_and_Conditions')
--- travel/models.py
+++ travel/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from datetime import datetime
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    choice = (('0', 'Female'), ('1', 'Male'), ('2', 'Other'))
+    college = models.CharField(max_length=100)
+    choice1 = (('Gurgaon','Gurgaon'),(),())
+    location = models.CharField(max_length=20)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+    gender = models.CharField(max_length=6, choices=choice)
+    contact = models.BigIntegerField(unique=True)
+    name = models.CharField(max_length=30)
+
+    def __unicode__(self):
+        return self.user.username
